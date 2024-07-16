68ce7d0d25841df64cfe36470132ae329cd0e325
vanparfait <nguemechiaparfait@gmail.com>
2024-07-16 09:20:17 +0100

    Ajout des donnees dans le formulaire

--- app/components/ToDoList.tsx
+++ app/components/ToDoList.tsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { prisma } from "../libs/prisma";
 
-const ToDoList = () => {
+const ToDoList = async () => {
+  const todos = await prisma.toDo.findMany({
+    orderBy: {
+      createAt: "desc",
+    },
+  });
+
   return (
     <div className="flex flex-col gap-3 mt-5">
-      <div className="flex gap-3 items-center p-2 border-b border-gray-300">
-        <input type="radio" name="todo" id="todo" />
-        <label htmlFor="todo">Faire le menage</label>
-      </div>
-      <div className="flex gap-3 items-center p-2 border-b border-gray-300">
-        <input type="radio" name="todo" id="todo" />
-        <label htmlFor="todo">Faire le menage</label>
-      </div>
-      <div className="flex gap-3 items-center p-2 border-b border-gray-300">
-        <input type="radio" name="todo" id="todo" />
-        <label htmlFor="todo">Faire le menage</label>
-      </div>
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className="flex gap-3 items-center p-2 border-b border-gray-300"
+        >
+          <input type="checkbox" name="todo" id="todo" />
+          <label htmlFor="todo">{todo.titre}</label>
+        </div>
+      ))}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,36 @@
-import Image from "next/image";
+import { revalidatePath } from "next/cache";
 import ToDoList from "./components/ToDoList";
+import { prisma } from "./libs/prisma";
 
 export default function Home() {
+  const addTodo = async (formData: FormData) => {
+    "use server";
+    const todo = formData.get("toDoItem");
+    await prisma.toDo.create({
+      data: {
+        titre: todo as string,
+        status: "A faire",
+      },
+    });
+    revalidatePath("/");
+    console.log("toDo Ajoute");
+  };
+
   return (
     <main className="bg-gray-100 flex justify-center items-center h-screen">
       <div className="w-96  bg-white p-3 shadow-md rounded-md min-h-80">
         <h1 className="text-lg font-bold text-center mb-6 underline">
           TODO-APP
         </h1>
-        <form action="" className="flex gap-3 items-center">
+        <form className="flex gap-3 items-center" action={addTodo}>
           {/* <label htmlFor="todo" className="text-sm">
               Ajouter une tache
             </label> */}
           <input
             id="todo"
             type="text"
-            name="todo"
+            name="toDoItem"
+            required
             className="border border-gray-300 p-2"
           />
 
